678f8cd567147ee8990e6e7e7f915578587f360c
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2021-03-29 01:12:14 +0200

    Added test files.

---
+++ TestProject/MainWindowTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    [TestClass]
+    public class MainWindowTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TestProject/Window1Tests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    [TestClass]
+    public class Window1Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
