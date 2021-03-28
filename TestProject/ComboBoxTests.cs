fa669fca1f5f777f87217f9d236bdf23d25c081d
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2021-03-29 01:55:03 +0200

    Added ComboBoxWindow functionallity.

---
+++ TestProject/ComboBoxTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+using TestableWpf;
+
+using WpfApp.Views;
+using WpfApp.ViewModels;
+
+namespace TestProject
+{
+    [TestClass]
+    public class ComboBoxTests
+    {
+        [TestMethod]
+        public void ComboBoxWindow_SetFruitByIndex_ReadFruitInTextBlock()
+        {
+            var window = ContentFactory.CreateWindow<ComboBoxWindow>();
+            var vm = new ComboBoxViewModel();
+
+            vm.Fruits = new List<string> { "Banana", "Pear", "Lemon", "Apple" };
+            window.DataContext = vm;
+            window.Show();
+
+            var cmbFruits = ControlFinder.FindByAutomationId<ComboBox>(window, "Fruits");
+            var txtFruit = ControlFinder.FindByAutomationId<TextBlock>(window, "Fruit");
+
+            var index = 1;
+            cmbFruits.SelectedIndex = index;
+            var expected = vm.Fruits[index];
+            var result = txtFruit.Text;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ComboBoxWindow_SetRandomFruitInViewModel_ReadFromSelectedItemAndTextBlock()
+        {
+            var window = ContentFactory.CreateWindow<ComboBoxWindow>();
+            var vm = new ComboBoxViewModel();
+
+            vm.Fruits = new List<string> { "Banana", "Pear", "Lemon", "Apple" };
+            window.DataContext = vm;
+            window.Show();
+
+            var cmbFruits = ControlFinder.FindByAutomationId<ComboBox>(window, "Fruits");
+            var txtFruit = ControlFinder.FindByAutomationId<TextBlock>(window, "Fruit");
+
+            var index = new Random().Next(0, vm.Fruits.Count);
+            var fruit = vm.Fruits[index];
+            vm.Fruit = fruit;
+            var result1 = cmbFruits.SelectedItem as string;
+            var result2 = txtFruit.Text;
+
+            Assert.AreEqual(fruit, result1);
+            Assert.AreEqual(fruit, result2);
+        }
+    }
+}
