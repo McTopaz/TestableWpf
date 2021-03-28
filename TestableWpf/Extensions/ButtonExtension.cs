1109ce29036dfd681784888f3ac5947ef1d9abdf
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2021-03-29 01:01:52 +0200

    Added extension methods.

---
+++ TestableWpf/Extensions/ButtonExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TestableWpf.Extensions
+{
+    public static class ButtonExtension
+    {
+        public static void ClickWithEvent(this Button button)
+        {
+            var clickEvent = System.Windows.Controls.Primitives.ButtonBase.ClickEvent;
+            var routedEventArgs = new RoutedEventArgs(clickEvent);
+            button.RaiseEvent(routedEventArgs);
+        }
+
+        public static void ClickWithCommand(this Button button, object parameter = null)
+        {
+            parameter = parameter is null ? button.CommandParameter : parameter;
+            button.Command.Execute(parameter);
+        }
+    }
+}
---
+++ TestableWpf/Extensions/TextBoxExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TestableWpf.Extensions
+{
+    public static class TextBoxExtension
+    {
+        public static void SendKeys(this TextBox textBox, string text)
+        {
+            var manager = InputManager.Current;
+            var composition = new TextComposition(manager, textBox, text);
+            TextCompositionManager.StartComposition(composition);
+        }
+    }
+}
