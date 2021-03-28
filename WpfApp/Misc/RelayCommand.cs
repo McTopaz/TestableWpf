9c8dcd12fed20d71b683112a25851bde498690e0
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2021-03-29 00:48:21 +0200

    Added basic components and controls.

---
+++ WpfApp/Misc/RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp.Misc
+{
+    public class RelayCommand : ICommand
+    {
+        public Action MethodToExecute { get; private set; }
+        Func<bool> CanExecuteEvaluator { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public RelayCommand(Action methodToExecute) : this(methodToExecute, null)
+        {
+        }
+
+        public RelayCommand(Action methodToExecute, Func<bool> canExecuteEvaluator)
+        {
+            this.MethodToExecute = methodToExecute;
+            this.CanExecuteEvaluator = canExecuteEvaluator;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteEvaluator is null)
+            {
+                return true;
+            }
+            else
+            {
+                return CanExecuteEvaluator.Invoke();
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            MethodToExecute.Invoke();
+        }
+    }
+}
---
+++ WpfApp/ViewModels/UserControl1ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PropertyChanged;
+
+namespace WpfApp.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class UserControl1ViewModel
+    {
+    }
+}
