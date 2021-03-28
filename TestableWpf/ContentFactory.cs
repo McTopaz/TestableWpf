798b7e33fc2b9373b7e41d1f2e79b5a278d2814f
Stefan Eriksson <stefan.k.j.eriksson@outlook.com>
2021-03-29 01:06:27 +0200

    Added ContentFactory.

---
+++ TestableWpf/ContentFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TestableWpf
+{
+    public class ContentFactory
+    {
+        static T CreateContent<T>() where T : DependencyObject
+        {
+            var type = typeof(T);
+            var content = Activator.CreateInstance(type);
+            return content as T;
+        }
+
+        public static T CreateWindow<T>() where T : Window
+        {
+            return CreateContent<T>();
+        }
+
+        public static Window CreateUserControl<T>() where T : UserControl
+        {
+            var uc = CreateContent<T>();
+            var host = new Window();
+            host.Content = uc;
+            return host;
+        }
+    }
+}
